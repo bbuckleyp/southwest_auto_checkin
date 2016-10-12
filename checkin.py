0e377363d22169c1ce1ce094ad461944d7744974
Brandon Buckley <bbuckleyp@gmail.com>
2016-10-11 17:42:05 -0700

    program mostly completed. Need final debug once in 24 hour window.

--- checkin.py
+++ checkin.py
@@ -2,16 +2,12 @@
 
 import requests
 import time
+import logging
 
 def checkin(first_name, last_name, confirmation_number):#, email):
     """
     Function call to checkin the user to their respective Southwest Flight.
     """
-
-    print first_name
-    print last_name
-    print confirmation_number
-
     # declaring variables
     payload1 = {'confirmationNumber': confirmation_number, 'firstName': first_name, 'lastName': last_name}
     payload2 = {'confirmationNumber': confirmation_number, 'firstName': first_name, 'lastName': last_name}
@@ -19,25 +15,32 @@ def checkin(first_name, last_name, confirmation_number):#, email):
     url1 = 'https://www.southwest.com/flight/retrieveCheckinDoc.html'
     url2 = 'https://www.southwest.com/flight/retrieveCheckinDoc.html'
 
+    print first_name
+    print last_name
+    print confirmation_number
+
+
     # opening session (also acquires cookie)
     s = requests.Session()
     s.get(url0)
+    logging.info('checkin.py: Session Open, Cookie Acquired')
 
     # checking in until successful
     success = 0
     while(success == 0):
-        # making POST request to southwest checkin form
+        # making most critical POST request to southwest checkin form
         r = s.post(url1, data=payload1)
+        logging.info('checkin.py: Checkin POST Made')
 
-        # check if checkin was successful
+        # if checkin successful then make final POST
         if "Boarding Pass is more than 24 hours prior" in r.text:
             #s.post(url2, data=payload2)
             print "User checked in successfully"
+            logging.info('checkin.py: User successfully checked in!')
             success = 1
         else: 
-            time.sleep(5)
+            print "Checkin failed...retrying"
+            logging.warning('Checkin failed...retrying')
+            time.sleep(1)
 
-    # output results to a file to keep track of data
-
-    # close session connection
     s.close()
--- schedule_flight.py
+++ schedule_flight.py
@@ -2,10 +2,20 @@
 
 import argparse
 import checkin
-from apscheduler.scheduler.BlockingScheduler import BlockingScheduler
+import logging
+import time
+import apscheduler.events as events
+from apscheduler.schedulers.background import BackgroundScheduler
 
+def sched_listener(event):
+    if event.code == events.EVENT_SCHEDULER_STARTED:
+        print "SCHEDULER was started"
+    elif event.code == events.EVENT_JOB_EXECUTED:
+        print "JOB was executed successfully"
+    elif event.code == events.EVENT_JOB_ERROR:
+        print "JOB raised an exception during execution"
 
-if __name__ == "__main__":
+def main():
     # Declaring Arguments to Parse
     parser = argparse.ArgumentParser(description="Checkin a user to a respective Southwest Airlines flight on a specified date")
     parser.add_argument("-n", "--now", action="store_true", help="Checkin the user right now")
@@ -17,12 +27,36 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
 
+    # Initializing logging
+    logging.basicConfig(filename='schedule_checkin.log', filemode='w', level=logging.DEBUG) 
+    logging.info('Started')
+    logging.info('Name: {} {}, Confirmation Number: {} Date-Time {} {}'.format(args.FirstName, args.LastName,
+        args.ConfirmationNumber, args.Date, args.Time))
+
+
+    # Checkin now or submit to scheduler
     if args.now:
         print "checking in now..."
+        logging.info('Checking in now')
         checkin.checkin(args.FirstName, args.LastName, args.ConfirmationNumber)
     else:
-        # scheduling based on date and time entered
-        sched = BlockingScheduler()
-        sch_date_time = args.Date + args.Time
+        # Initializing variables for apscheduler
+        sched = BackgroundScheduler()
+        sch_date_time = args.Date + " " + args.Time
         user_info = [args.FirstName, args.LastName, args.ConfirmationNumber]
+
+        logging.info('Scheduling checkin time/date')
+
+        # scheduling job
+        sched.add_listener(sched_listener)
         sched.add_job(checkin.checkin, args = user_info, trigger='date', next_run_time=sch_date_time)
+        sched.start()
+
+    # Checking if job was completed, if so then exit
+    while sched.get_jobs():
+        time.sleep(10)
+
+    logging.info('Finished')
+
+if __name__ == "__main__":
+    main()
